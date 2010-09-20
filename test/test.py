ec03fb53e5ce6fc5ca2f460c3d8db0fbc848b77e
Trent Mick <trentm@gmail.com>
2010-09-20 18:38:30 +0000

    get "python test.py" to work

--- test/test.py
+++ test/test.py
@@ -14,12 +14,13 @@ finally:
 
 
 def setup():
-    cmdln_src_dir = dirname(dirname(abspath(__file__)))
+    cmdln_lib_dir = join(dirname(dirname(abspath(__file__))), "lib")
     if "PYTHONPATH" in os.environ:
         os.environ["PYTHONPATH"] = os.pathsep.join(
-            [cmdln_src_dir, os.environ["PYTHONPATH"]])
+            [cmdln_lib_dir, os.environ["PYTHONPATH"]])
     else:
-        os.environ["PYTHONPATH"] = cmdln_src_dir
+        os.environ["PYTHONPATH"] = cmdln_lib_dir
+    sys.path.insert(0, cmdln_lib_dir)
 
 #TODO: hook test_doctests.py into this
 if __name__ == "__main__":
