98d5c8bd8463a7a124f551c98b6aac3644d548af
Trent Mick <trentm@gmail.com>
2015-04-10 22:35:33 -0700

    py3 print function for docs/conan.py

--- docs/conan.py
+++ docs/conan.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import sys
 import textwrap
 
@@ -15,8 +16,8 @@ class Conan(cmdln.Cmdln):
     name = "conan"
     def do_hello(self, subcmd, opts):
         """${cmd_name}: Conan greets thee"""
-        print "Ugh!"
-    
+        print("Ugh!")
+
     @cmdln.option("-w", "--weapon", help="what weapon should Conan use?")
     def do_crush(self, subcmd, opts, *enemies):
         """${cmd_name}: crush your enemies!
@@ -32,19 +33,19 @@ class Conan(cmdln.Cmdln):
             "maul": "Crush",
         }.get(opts.weapon, None)
         if not action:
-            print "Conan confused."
+            print("Conan confused.")
         else:
             for enemy in enemies:
-                print "%s %s!" % (action, enemy)
-            print "Yargh!"
+                print("%s %s!" % (action, enemy))
+            print("Yargh!")
 
     @cmdln.alias("what_is_best", "best")
     def do_what_is_best_in_life(self, subcmd, opts):
         """${cmd_name}: Big monologue"""
-        print textwrap.dedent("""\
+        print(textwrap.dedent("""\
             To crush your enemies,
             see them driven before you,
-            and hear the lamentations of the women.""")
+            and hear the lamentations of the women."""))
 
 if __name__ == "__main__":
     conan = Conan()
